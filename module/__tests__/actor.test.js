acd40751149370f4e779abce73c3064c7888350c
Tim White <tim@cyface.com>
2020-07-20 22:57:55 -0600

    Basic tests with Foundry mocks.

--- module/__tests__/actor.test.js
+++ module/__tests__/actor.test.js
@@ -32,4 +32,14 @@ test('roll ability scores', () => {
   actor.rollAbilityCheck('str')
   expect(Roll).toHaveBeenCalledTimes(1)
   expect(Roll).toHaveBeenCalledWith('1d20+@abilMod', { 'abilMod': -1, 'critical': 20 })
+
+  //Check that luck doesn't add ability mod
+  actor.rollAbilityCheck('lck', { event: { currentTarget: { className: 'bob' } } })
+  expect(Roll).toHaveBeenCalledTimes(3)
+  expect(Roll).toHaveBeenCalledWith('1d20')
+
+  //Unless they click on the ability mod of luck
+  actor.rollAbilityCheck('lck', { event: { currentTarget: { className: 'ability-modifiers' } } })
+  expect(Roll).toHaveBeenCalledTimes(4)
+  expect(Roll).toHaveBeenCalledWith('1d20+@abilMod', { 'abilMod': -1, 'critical': 20 })
 })
