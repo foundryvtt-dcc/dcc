63142b6a4b08f83d6ea7d28b523945ea5b9d1191
Tim L. White <twhite@fool.com>
2020-07-25 15:01:09 -0600

    Mocked Test for Foundry Objects (#15)
    * Adding images to Compendium.
    
    * Adding images to Compendium.
    
    * Basic tests with Foundry mocks.
    
    * Adding images to Compendium.
    
    * Basic tests for mocked Foundry
    
    * Tests for foundry mocks
    
    * Saving throw test
    
    * async/await updates

---
+++ module/__mocks__/foundry.js
@@ -0,0 +1,102 @@
+console.log('Loading Foundry Mocks')
+
+import DCC from '../config.js'
+
+/**
+ * Actor
+ */
+class Actor {
+  constructor (data, options) {
+    // If test-specific data is passed in use it, otherwise use default data
+    if (data) {
+      this.data = data
+    } else {
+      this.data = {
+        'data': {
+          'abilities': {
+            'str': { 'value': 6 },
+            'agl': { 'value': 8 },
+            'sta': { 'value': 12 },
+            'int': { 'value': 14 },
+            'per': { 'value': 16 },
+            'lck': { 'value': 18 }
+          },
+          'saves': {
+            'frt': { 'value': -1 },
+            'ref': { 'value': 0 },
+            'wil': { 'value': +12 },
+          }
+        }
+      }
+    }
+    this.prepareData()
+  }
+
+  prepareData () {
+    console.log('Mock Actor: super prepareData was called')
+  }
+}
+
+global.actor = new Actor()
+global.Actor = Actor
+
+/**
+ * ChatMessage
+ */
+class ChatMessage {
+  constructor (data, options) {
+    // If test-specific data is passed in use it, otherwise use default data
+    if (data) {
+      this.data = data
+    }
+  }
+
+  static getSpeaker ({ scene, actor, token, alias } = {}) {
+    return actor
+  }
+}
+
+global.ChatMessage = ChatMessage
+
+/**
+ * CONFIG
+ */
+global
+  .CONFIG = { 'DCC': DCC }
+
+/**
+ * Localization
+ */
+class Localization {
+  localize (stringId) {
+    //Just strip the DCC off the string ID to simulate the lookup
+    return stringId.replace('DCC.', '')
+  }
+}
+
+global.Localization = Localization
+
+/**
+ * Game
+ */
+class Game {
+  constructor (worldData, sessionId, socket) {
+    this.i18n = new Localization()
+  }
+}
+
+global.Game = Game
+global.game = new Game()
+
+/**
+ * Roll
+ */
+global.rollToMessageMock = jest.fn((data) => {
+  console.log('Mock Roll: toMessage was called with:')
+  console.log(data)
+})
+global.Roll = jest.fn(() => {
+  return {
+    toMessage: rollToMessageMock
+  }
+}).mockName('Roll');
---
+++ module/__tests__/actor.test.js
@@ -0,0 +1,69 @@
+/* Tests for Actor.js using Foundry Mocks */
+/* Mocks for Foundry Classes/Functions are found in __mocks__/foundry.js */
+
+import DCCActor from '../actor'
+
+//Create Base Test Actor
+const actor = new DCCActor()
+
+test('prepareData sets ability modifiers', () => {
+  const abilities = actor.data.data.abilities
+
+  expect(abilities.str.value).toEqual(6)
+  expect(abilities.str.mod).toEqual(-1)
+
+  expect(abilities.agl.value).toEqual(8)
+  expect(abilities.agl.mod).toEqual(-1)
+
+  expect(abilities.sta.value).toEqual(12)
+  expect(abilities.sta.mod).toEqual(0)
+
+  expect(abilities.int.value).toEqual(14)
+  expect(abilities.int.mod).toEqual(1)
+
+  expect(abilities.per.value).toEqual(16)
+  expect(abilities.per.mod).toEqual(2)
+
+  expect(abilities.lck.value).toEqual(18)
+  expect(abilities.lck.mod).toEqual(3)
+})
+
+test('roll ability check', () => {
+  actor.rollAbilityCheck('str')
+  expect(Roll).toHaveBeenCalledTimes(1)
+  expect(Roll).toHaveBeenCalledWith('1d20+@abilMod', { 'abilMod': -1, 'critical': 20 })
+  expect(rollToMessageMock).toHaveBeenCalledWith({ flavor: 'AbilityStr Check', speaker: actor })
+
+  //Check that luck doesn't add ability mod
+  actor.rollAbilityCheck('lck', { event: { currentTarget: { className: 'random' } } })
+  expect(Roll).toHaveBeenCalledTimes(3) //called once above, then set for default lck then reset for luck check
+  expect(Roll).toHaveBeenCalledWith('1d20')
+  expect(rollToMessageMock).toHaveBeenLastCalledWith({ flavor: 'AbilityLck Check', speaker: actor })
+
+  //Unless they click on the ability mod of luck
+  actor.rollAbilityCheck('lck', { event: { currentTarget: { className: 'ability-modifiers' } } })
+  expect(Roll).toHaveBeenCalledTimes(4)
+  expect(Roll).toHaveBeenCalledWith('1d20+@abilMod', { 'abilMod': -1, 'critical': 20 })
+  expect(rollToMessageMock).toHaveBeenLastCalledWith({ flavor: 'AbilityLck Check', speaker: actor })
+
+  Roll.mockClear()
+})
+
+test('roll saving throw', () => {
+  actor.rollSavingThrow('frt')
+  expect(Roll).toHaveBeenCalledTimes(1)
+  expect(Roll).toHaveBeenCalledWith('1d20+@saveMod', { 'saveMod': -1})
+  expect(rollToMessageMock).toHaveBeenCalledWith({ flavor: 'SavesFortitude Save', speaker: actor })
+
+  actor.rollSavingThrow('ref')
+  expect(Roll).toHaveBeenCalledTimes(2)
+  expect(Roll).toHaveBeenCalledWith('1d20+@saveMod', { 'saveMod': 0})
+  expect(rollToMessageMock).toHaveBeenCalledWith({ flavor: 'SavesReflex Save', speaker: actor })
+
+  actor.rollSavingThrow('wil')
+  expect(Roll).toHaveBeenCalledTimes(3)
+  expect(Roll).toHaveBeenCalledWith('1d20+@saveMod', { 'saveMod': +12})
+  expect(rollToMessageMock).toHaveBeenCalledWith({ flavor: 'SavesWill Save', speaker: actor })
+
+  Roll.mockClear()
+})
