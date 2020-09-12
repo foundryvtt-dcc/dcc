1d2d6c522864857d5c6508a14dd2b52f466399d0
Steve Barnett <mooped@gmail.com>
2020-09-12 14:53:57 +0100

    Implement some fundamental low level functions in the Foundry mocks so the system under test operates correctly.

--- module/__mocks__/foundry.js
+++ module/__mocks__/foundry.js
@@ -231,8 +231,135 @@ global.ui = {
 /**
  * Global helper functions function
  */
-// TODO: This really needs to be the real Foundry mergeObject function or there will be side effects that make the tests invalid
-global.mergeObject = function (original, other = {}) {
+
+// Foundry's implementation of getType
+global.getType = function (token) {
+  const tof = typeof token
+  if (tof === 'object') {
+    if (token === null) return 'null'
+    const cn = token.constructor.name
+    if (['String', 'Number', 'Boolean', 'Array', 'Set'].includes(cn)) return cn
+    else if (/^HTML/.test(cn)) return 'HTMLElement'
+    else return 'Object'
+  }
+  return tof
+}
+
+// Foundry's implementation of setProperty
+global.setProperty = function (object, key, value) {
+  let target = object
+  let changed = false
+
+  // Convert the key to an object reference if it contains dot notation
+  if (key.indexOf('.') !== -1) {
+    const parts = key.split('.')
+    key = parts.pop()
+    target = parts.reduce((o, i) => {
+      if (!Object.prototype.hasOwnProperty.call(o, i)) o[i] = {}
+      return o[i]
+    }, object)
+  }
+
+  // Update the target
+  if (target[key] !== value) {
+    changed = true
+    target[key] = value
+  }
+
+  // Return changed status
+  return changed
+}
+
+// Foundry's implementation of expandObject
+global.expandObject = function (obj, _d = 0) {
+  const expanded = {}
+  if (_d > 10) throw new Error('Maximum depth exceeded')
+  for (let [k, v] of Object.entries(obj)) {
+    if (v instanceof Object && !Array.isArray(v)) v = global.expandObject(v, _d + 1)
+    global.setProperty(expanded, k, v)
+  }
+  return expanded
+}
+
+// Foundry's implementation of duplicate
+global.duplicate = function (original) {
+  return JSON.parse(JSON.stringify(original))
+}
+
+// Foundry's implementation of mergeObject
+global.mergeObject = function (original, other = {}, { insertKeys = true, insertValues = true, overwrite = true, recursive = true, inplace = true, enforceTypes = false } = {}, _d = 0) {
+  other = other || {}
+  if (!(original instanceof Object) || !(other instanceof Object)) {
+    throw new Error('One of original or other are not Objects!')
+  }
+  const depth = _d + 1
+
+  // Maybe copy the original data at depth 0
+  if (!inplace && (_d === 0)) original = global.duplicate(original)
+
+  // Enforce object expansion at depth 0
+  if ((_d === 0) && Object.keys(original).some(k => /\./.test(k))) original = global.expandObject(original)
+  if ((_d === 0) && Object.keys(other).some(k => /\./.test(k))) other = global.expandObject(other)
+
+  // Iterate over the other object
+  for (let [k, v] of Object.entries(other)) {
+    const tv = global.getType(v)
+
+    // Prepare to delete
+    let toDelete = false
+    if (k.startsWith('-=')) {
+      k = k.slice(2)
+      toDelete = (v === null)
+    }
+
+    // Get the existing object
+    let x = original[k]
+    let has = Object.prototype.hasOwnProperty.call(original, k)
+    let tx = global.getType(x)
+
+    // Ensure that inner objects exist
+    if (!has && (tv === 'Object')) {
+      x = original[k] = {}
+      has = true
+      tx = 'Object'
+    }
+
+    // Case 1 - Key exists
+    if (has) {
+      // 1.1 - Recursively merge an inner object
+      if ((tv === 'Object') && (tx === 'Object') && recursive) {
+        global.mergeObject(x, v, {
+          insertKeys: insertKeys,
+          insertValues: insertValues,
+          overwrite: overwrite,
+          inplace: true,
+          enforceTypes: enforceTypes
+        }, depth)
+
+      // 1.2 - Remove an existing key
+      } else if (toDelete) {
+        delete original[k]
+
+      // 1.3 - Overwrite existing value
+      } else if (overwrite) {
+        if (tx && (tv !== tx) && enforceTypes) {
+          throw new Error('Mismatched data types encountered during object merge.')
+        }
+        original[k] = v
+
+      // 1.4 - Insert new value
+      } else if ((x === undefined) && insertValues) {
+        original[k] = v
+      }
+
+    // Case 2 - Key does not exist
+    } else if (!toDelete) {
+      const canInsert = (depth === 1 && insertKeys) || (depth > 1 && insertValues)
+      if (canInsert) original[k] = v
+    }
+  }
+
+  // Return the object for use
   return original
 }
 
