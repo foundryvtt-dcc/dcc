89063e900d38df853a3903dbaa66cb3ae22f3900
Steve Barnett <mooped@gmail.com>
2021-03-09 22:07:17 +0000

    Remove non-functional Shortened Macro Syntax setting.

--- module/settings.js
+++ module/settings.js
@@ -126,16 +126,4 @@ export const registerSystemSettings = async function () {
     default: false,
     config: true
   })
-
-  /**
-   * Macro Shorthand setting
-   */
-  game.settings.register('dcc', 'macroShorthand', {
-    name: 'Shortened Macro Syntax',
-    hint: 'Enable a shortened macro syntax which allows referencing attributes directly, for example @str instead of @attributes.str.value. Disable this setting if you need the ability to reference the full attribute model, for example @attributes.str.label.',
-    scope: 'world',
-    type: Boolean,
-    default: true,
-    config: true
-  })
 }
