7da4a91493c6b776625232acee0c08188c7cbb16
Steve Barnett <mooped@gmail.com>
2021-04-20 22:02:36 +0100

    Handle negative speed (for armor penalty) and format in feet on the armor sheet.

--- module/dcc.js
+++ module/dcc.js
@@ -102,7 +102,7 @@ Hooks.once('init', async function () {
 
   // Handlebars helper for distances with an apostrophe
   Handlebars.registerHelper('distanceFormat', function (object) {
-    const fields = String(object).match(/(\d+)'?/)
+    const fields = String(object).match(/(-?\d+)'?/)
     if (fields) {
       return fields[1] + '\''
     } else {
