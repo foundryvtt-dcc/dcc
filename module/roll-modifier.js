c50c3be81134702fe80f44058c9e8e1e6c5df7a9
Steve Barnett <mooped@gmail.com>
2021-08-06 17:26:14 +0100

    Progress on a Roll Modifier dialog that can be customised by callers.

--- module/roll-modifier.js
+++ module/roll-modifier.js
@@ -1,17 +1,20 @@
 /* global CONFIG, FormApplication, game */
 
 class RollModifierDialog extends FormApplication {
-  constructor (options = {}) {
+  constructor (resolve, reject, roll, options = {}) {
     super()
-    this.options = options
+    this._roll = roll
+    this._resolve = resolve
+    this._reject = reject
+    Object.assign(this.options, options)
   }
 
   static get defaultOptions () {
     const options = super.defaultOptions
     options.id = 'dcc-roll-modifier'
-    options.width = 800
-    options.height = 400
-    options.template = CONFIG.DCC.templates.rollModifiers
+    options.width = 400
+    options.height = 250
+    options.template = CONFIG.DCC.templates.rollModifierDialog
     return options
   }
 
@@ -20,11 +23,15 @@ class RollModifierDialog extends FormApplication {
    * @type {string}
    */
   get title () {
-    return game.i18n.localize('DCC.DisapprovalRollFormula')
+    return this.options.title || game.i18n.localize('DCC.RollModifierTitle')
   }
 
+  /*
+   * The roll object being edited
+   * @type {Roll}
+   */
   get roll () {
-    return this.options.roll
+    return this._roll
   }
 
   /* -------------------------------------------- */
@@ -38,7 +45,19 @@ class RollModifierDialog extends FormApplication {
     data.user = game.user
     data.roll = this.roll
     data.options = this.options
+    data.terms = this._extractTerms(this.roll)
     return data
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners (html) {
+    super.activateListeners(html)
+
+    html.find('button.cancel').click(this._onCancel.bind(this))
+    html.find('button.dice-chain').click(this._modifyDie.bind(this))
+    html.find('button.bonus').click(this._modifyBonus.bind(this))
   }
 
   /**
@@ -50,52 +69,137 @@ class RollModifierDialog extends FormApplication {
   async _updateObject (event, formData) {
     event.preventDefault()
 
-    // Set the showWelcomeDialog setting according to the form's checkbox
-    game.settings.set('dcc-core-book', 'showWelcomeDialog', !formData['data.doNotShow'])
+    // Build a new roll from the collected terms
+    const formula = ''
+    this.element.find('input.term-field').each((index, element) => {
+      formula += element.val()
+    })
+    this._roll = new Roll(formula)
+
+    this._resolve(this.roll)
   }
 
   /**
-   * Handle the XXX button
+   * Handle the Cancel button
    * @param event {Event}       The originating click event
    * @private
    */
-  async _onClickSomeButton () {
+  async _onCancel (event) {
+    event.preventDefault()
+    this._reject(null)
+    super.close()
+  }
+
+  /**
+   * Modify a Die term
+   * @param event {Event}  The originating click event
+   * @private
+   */
+  async _modifyDie (event) {
+    event.preventDefault()
+    const index = event.currentTarget.dataset.term
+    const mod = event.currentTarget.dataset.mod
+    const formField = this.element.find('#term-' + index)
+    const termFormula = formField.val()
+  }
+
+  /**
+   * Modify a non-Die term
+   * @param event {Event}  The originating click event
+   * @private
+   */
+  async _modifyBonus (event) {
+    event.preventDefault()
+    const index = event.currentTarget.dataset.term
+    const mod = event.currentTarget.dataset.mod
+    const formField = this.element.find('#term-' + index)
+    let termFormula = formField.val()
+    termFormula = (parseInt(termFormula) + parseInt(mod)).toString()
+    if (termFormula[0] !== '-') {
+      termFormula = '+' + termFormula
+    }
+    formField.val(termFormula)
+  }
+
+  /** @override */
+  async close (options = {}) {
+    super.close(options)
+    this._reject(null)
+  }
+
+  /**
+   * Extract the terms from a roll into Dice and Modifiers and add any extra terms
+   * @private
+   */
+  _extractTerms () {
+    // State
+    const terms = []
+    let termAccumulator = ''
+    let lastTerm = null
+    let index = 0
+
+    // Helper functions
+    var addDieTerm = function (term) {
+      terms.push({
+        type: 'Die',
+        label: term.options.flavor || game.i18n.localize('DCC.RollModifierDieTerm'),
+        index: index++,
+        isDie: true,
+        formula: term.formula.replace(' ', '')
+      })
+      termAccumulator = ''
+    }
+    var addModifierTerm = function (term) {
+      terms.push({
+        type: 'Modifiers',
+        label: term.options.flavor || game.i18n.localize('DCC.RollModifierModifierTerm'),
+        index: index++,
+        isDie: false,
+        formula: termAccumulator.replace(/\+\+/g, '+').replace(/--/g, '+').replace(/\+-/g, '-').replace(/-\+/g, '-')
+      })
+      termAccumulator = ''
+    }
+    var accumulateTerm = function (term) {
+      termAccumulator += term.formula.replace(/\s+/g, '')
+      lastTerm = term
+    }
+  
+    // Extract terms from the Roll
+    for (const term of this.roll.terms) {
+      if (term instanceof Die) {
+        // Isolate Die terms
+        if (termAccumulator) {
+          addModifierTerm(term)
+        }
+        addDieTerm(term)
+      } else {
+        // Accumulate and concatenate non-die terms
+        accumulateTerm(term)
+      }
+    }
+    if (termAccumulator) {
+      // Mop up any remaining modifiers
+      addModifierTerm(lastTerm)
+    }
+
+    // Add any extra terms from the options
+    if (this.options.extraTerms) {
+      for (const key in this.options.extraTerms) {
+        const term = this.options.extraTerms[key]
+        terms.push(Object.assign(term, {
+          index: index++
+        }))
+      }
+    }
+
+    return terms
   }
 }
 
 async function showRollModifier (roll, options) {
-  const html = await renderTemplate(CONFIG.DCC.templates.rollModifiers, {
-    roll,
-    options
-  })
   return new Promise((resolve, reject) => {
-    new Dialog({
-      title: game.i18n.localize('DCC.DisapprovalRollFormula'),
-      content: html,
-      buttons: {
-        yes: {
-          icon: '<i class="fas fa-check"></i>',
-          label: game.i18n.localize('DCC.RollModifierRoll'),
-          callback: html => {
-            const formula = html[0].querySelector('#modify-roll-form')[0].value
-            resolve(_modifyRoll(roll, formula))
-          }
-        },
-        no: {
-          icon: '<i class="fas fa-times"></i>',
-          label: game.i18n.localize('DCC.Cancel'),
-          callback: () => {
-            resolve(roll)
-          }
-        }
-      },
-      close: () => { resolve(roll) }
-    }).render(true)
+    new RollModifierDialog(resolve, reject, roll, options).render(true)
   })
-}
-
-function _modifyRoll (roll, formula) {
-  return new Roll(formula)
 }
 
 export default showRollModifier
