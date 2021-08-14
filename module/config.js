4949ae962a97220adee0043af96afde92506f640
Steve Barnett <mooped@gmail.com>
2021-08-14 11:48:47 +0100

    Create a Party Sheet actor type, sheet class, and template.

--- module/config.js
+++ module/config.js
@@ -552,7 +552,8 @@ DCC.skillTables = {
 
 // Default actor images
 DCC.defaultActorImages = {
-  default: 'systems/dcc/styles/images/actor.webp'
+  default: 'systems/dcc/styles/images/actor.webp',
+  Party: 'systems/dcc/styles/images/party.webp'
 }
 
 // Default item tokens
