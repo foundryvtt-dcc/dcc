581a1fa234c327eceb39fedbfef3bd3745f4ce6e
Tim <tim@cyface.com>
2024-08-02 21:38:07 -0600

    Add roll terms to roll mock.

--- module/__mocks__/roll.js
+++ module/__mocks__/roll.js
@@ -20,12 +20,26 @@ global.rollEvaluateMock = jest.fn(() => {
 global.rollValidateMock = jest.fn((formula) => {
   return true
 })
+global.rollTermsMock = [
+  {
+    "class": "Die",
+    "options": {
+      "flavor": null
+    },
+    "evaluated": false,
+    "number": 1,
+    "faces": 20,
+    "modifiers": [],
+    "results": []
+  }
+]
 const Roll = jest.fn((formula, data = {}) => {
   return {
     dice: [{ results: [10], options: {} }],
     toMessage: global.rollToMessageMock,
     evaluate: global.rollEvaluateMock,
-    roll: global.rollEvaluateMock
+    roll: global.rollEvaluateMock,
+    terms: global.rollTermsMock
   }
 }).mockName('Roll')
 global.Roll = Roll
