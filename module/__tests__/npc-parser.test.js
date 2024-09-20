62bb033a73da00978a7aaaa4dac2cbebb037eafa
Tim White <twhite@fool.com>
2024-09-20 15:09:40 -0600

    Adjustment for certain monster HD.

--- module/__tests__/npc-parser.test.js
+++ module/__tests__/npc-parser.test.js
@@ -6,7 +6,7 @@
 import parseNPCs from '../npc-parser.js'
 
 /* Test snake */
-test('super snake', async() => {
+test('super snake', async () => {
   const parsedNPC = await parseNPCs('Very long, the power super-snake: Init +0; Atk bite +6 melee; Dmg 1d8;\r\n AC 13; HP 21; MV 20’; Act 1d20; SV Fort +8, Ref +4, Will +4; AL L.')
   const expected = {
     name: 'Very long, the power super-snake',
@@ -38,7 +38,7 @@ test('super snake', async() => {
 })
 
 /* Test dry pile of bones */
-test('pile of bones', async() => {
+test('pile of bones', async () => {
   const parsedNPC = await parseNPCs('Seven items of dry stuff: Init -2; Atk bite +0 melee; Dmg 1d4-1; AC 8; HP 3; MV 5’;\r\n Act 1d20; SV Fort +0, Ref -4, Will +1; AL C.')
   const expected = {
     name: 'Seven items of dry stuff',
@@ -67,7 +67,7 @@ test('pile of bones', async() => {
 })
 
 /* Test orcs */
-test('orcs', async() => {
+test('orcs', async () => {
   const parsedNPC = await parseNPCs('Cute-Infused Orcs (3): Init +2; Atk claw +1 melee (1d4) or spear +1 melee (1d8); AC 15; HD 2d8+2; hp 13 each; MV 30’; Act 1d20; SP none; SV Fort +3, Ref +0, Will -1; AL C.')
   const expected = {
     name: 'Cute-Infused Orcs',
@@ -112,7 +112,7 @@ test('orcs', async() => {
 })
 
 /* Test spider */
-test('spider', async() => {
+test('spider', async () => {
   const parsedNPC = await parseNPCs('Xformed, Unicorn-Filled Spider: Init +1; Atk bite +2 melee (1d4 plus poison) or web +4 ranged (restrained, 20’ range); AC 13; HD 2d12 +2; hp 20; MV 30’ or climb 30’; Act 1d20; SP poison (DC 14 Fort save or additional 3d4 damage and lose 1 point of Strength, 1d4 damage if successful), create web, filled with bats; SV Fort +2, Ref +4, Will +0; AL N.\n')
   const expected = {
     name: 'Xformed, Unicorn-Filled Spider',
@@ -160,7 +160,7 @@ test('spider', async() => {
 })
 
 /* Test wetad */
-test('wedad', async() => {
+test('wedad', async () => {
   const parsedNPC = await parseNPCs('Gerieah (in her tree): Init +1; Atk tree limb slam +5 melee (1d10); AC 15;\n HD 4d10; hp 30; MV none; Act 1d20; SP takes 2x damage from fire, can attack targets up to 20’ away with tree limbs; SV Fort +6, Ref -2, Will +4; AL N.')
   const expected = {
     name: 'Gerieah (in her tree)',
@@ -194,7 +194,7 @@ test('wedad', async() => {
 })
 
 /* Test smultist */
-test('smultist', async() => {
+test('smultist', async () => {
   const parsedNPC = await parseNPCs('Green-robed smultist (1): Init +4; Atk dagger +5 melee (1d4+3); AC 11; HD 5d4+5; hp 21; MV 20’; SP 3d6 control check, able to cast arms of the angel, squid-mass (when killed, an squid-mass emerges; see stats below); Act 1d20; SV Fort +3, Ref +4, Will +0; AL C. Equipment: bird-shaped talisman of gold tied on a leather thong (worth 10 gp; see level 3).')
   const expected = {
     name: 'Green-robed smultist',
@@ -228,13 +228,13 @@ test('smultist', async() => {
 })
 
 /* Test short statline */
-test('shortstats', async() => {
+test('shortstats', async () => {
   const parsedNPC = await parseNPCs('Stunty, the short and muddled: Init +1; Atk kick +2 melee (1d3); AC 15;\n hp 4; Act 1d20; SV Ref +6, Fort -2, Will +4.')
   const expected = {
     name: 'Stunty, the short and muddled',
     'attributes.init.value': '+1',
     'attributes.ac.value': '15',
-    'attributes.hitDice.value': '1',
+    'attributes.hitDice.value': '1d4',
     'attributes.hp.value': '4',
     'attributes.hp.max': '4',
     'attributes.speed.value': '30',
@@ -262,13 +262,13 @@ test('shortstats', async() => {
 })
 
 /* Test the bad guy's familiar with a minimal stat line */
-test('familiar', async() => {
+test('familiar', async () => {
   const parsedNPC = await parseNPCs('The bad guy\'s familiar: Atk claw +3 melee (1d4), AC 15, HP 2.')
   const expected = {
     name: 'The bad guy\'s familiar',
     'attributes.init.value': '+0',
     'attributes.ac.value': '15',
-    'attributes.hitDice.value': '1',
+    'attributes.hitDice.value': '1d4',
     'attributes.hp.value': '2',
     'attributes.hp.max': '2',
     'attributes.speed.value': '30',
@@ -296,7 +296,7 @@ test('familiar', async() => {
 })
 
 /* Test damage modifiers */
-test('bonusguy', async() => {
+test('bonusguy', async () => {
   const parsedNPC = await parseNPCs('Bonus Guy: Init -1; Atk big club +3 melee (1d4+2) or small club -2 melee (1d4 - 3); AC 13; HD 1d8+2; MV 30’; Act 1d20; SV Fort +2, Ref +1, Will -2; AL C.')
   const expected = {
     name: 'Bonus Guy',
@@ -338,9 +338,9 @@ test('bonusguy', async() => {
 })
 
 /* Test multiple statlines */
-test('rodentsquad', async() => {
+test('rodentsquad', async () => {
   const parsedNPC = await parseNPCs(
-    `Mega Mole: Init +5; Atk claws +6 melee (1d8+3) ; AC 17;
+          `Mega Mole: Init +5; Atk claws +6 melee (1d8+3) ; AC 17;
 HD 3d8; hp 16; MV 20’; Act 1d20; SV Fort +4, Ref +4, Will +2;
 AL C.
 
@@ -431,6 +431,60 @@ Ref +4, Will +2; AL C.`
             toHit: '+2',
             damage: '1d3-1',
             melee: true
+          }
+        }
+      ]
+    }
+  ]
+  expect(parsedNPC).toMatchObject(expected)
+})
+
+/* Test giant stat block */
+test('giant', async () => {
+  const parsedNPC = await parseNPCs(
+          `Gabbie (stone giant): Init +1; Atk club +18 melee (3d8+10)
+or hurled stone +10 missile fire (1d8+10, range 200’); Crit
+20-24 G/d4; AC 17; HD 12d10 (hp 72); MV 40’; Act 1d24; SP
+infravision 60’, stone camouflage, transmute earth; SV Fort
++12, Ref +6, Will +8; AL N.`
+  )
+  const expected = [
+    {
+      name: 'Gabbie (stone giant)',
+      'attributes.init.value': '+1',
+      'attributes.ac.value': '17',
+      'attributes.hitDice.value': '12d10',
+      'attributes.speed.value': '40’',
+      'config.actionDice': '1d24',
+      'saves.frt.value': '+12',
+      'saves.ref.value': '+6',
+      'saves.wil.value': '+8',
+      'details.alignment': 'n',
+      items: [
+        {
+          name: 'club',
+          type: 'weapon',
+          img: 'systems/dcc/styles/images/weapon.webp',
+          system: {
+            actionDie: '1d20',
+            backstab: false,
+            backstabDamage: null,
+            toHit: '+18',
+            damage: '3d8+10',
+            melee: true
+          }
+        },
+        {
+          name: 'hurled stone',
+          type: 'weapon',
+          img: 'systems/dcc/styles/images/weapon.webp',
+          system: {
+            actionDie: '1d20',
+            backstab: false,
+            backstabDamage: null,
+            toHit: '+10',
+            damage: '1d8+10',
+            melee: false
           }
         }
       ]
