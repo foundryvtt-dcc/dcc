161d78b4892a11ecf28adf37620d066b59b15710
Steve Barnett <mooped@gmail.com>
2024-11-18 20:59:03 +0000

    Fix hotbar drop for non-items.

--- module/dcc.js
+++ module/dcc.js
@@ -498,7 +498,7 @@ async function processSpellCheck (actor, spellData) {
 /* -------------------------------------------- */
 // Create a macro when a rollable is dropped on the hotbar
 Hooks.on('hotbarDrop', (bar, data, slot) => {
-  createDCCMacro(data, slot)
+  return createDCCMacro(data, slot)
 })
 
 // Highlight 1's and 20's for all regular rolls, special spell check handling
